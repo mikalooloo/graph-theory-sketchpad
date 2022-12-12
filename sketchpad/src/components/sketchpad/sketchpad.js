891e6463452e0d4c34b4add2a3a85089d05fd9c4
mikalooloo <mikaeladean10@gmail.com>
2022-12-12 01:13:55 -0800

    mostly working with labels

--- sketchpad/src/components/sketchpad/sketchpad.js
+++ sketchpad/src/components/sketchpad/sketchpad.js
@@ -3,24 +3,87 @@ import '../aesthetic/sketchpad.css';
 import React from 'react';
 import { ThemeContext } from '../aesthetic/theme';
 
-import { Stage, Layer, Group, Circle, Rect, Line } from 'react-konva';
+import { Stage, Layer, Group, Circle, Rect, Line, Text } from 'react-konva';
 
 /*
  * Sketchpad where vertices and edges can be fiddled with
  */
-export default function Sketchpad({ mode, height, width }) {
+export default function Sketchpad({ mode, color, setVertexCount, setEdgeCount, height, width }) {
     // Theme and background
     const theme = React.useContext(ThemeContext).theme;
 
-    const Background = ({ color }) => {
+    const Background = ({ bgColor }) => {
+        // height and width are minus six because for some reason it gets drawn over the border instead of underneath if not done
         return <Rect
             x={0}
             y={0}
-            height={height}
-            width={width}
-            fill={color}
-            onMouseDown={onVertexCreate}
+            height={height - 6}
+            width={width - 6}
+            fill={bgColor}
+            onMouseDown={onCanvasClick}
         />
+    }
+
+    const [bgColor, setBGColor] = React.useState(theme === "dark" ? "rgb(100, 100, 100)" : "white");
+
+    const onCanvasClick = (e) => {
+        switch (mode) {
+            case "vertex":
+                if (e.evt.button === 0) {
+                    onVertexCreate(e);
+                }
+                break;
+            case "color":
+                setBGColor(color);
+                break;
+            default:
+                break;
+        }
+    }
+
+    const onVertexClick = (e) => {
+        switch (mode) {
+            case "vertex":
+                if (e.evt.button === 1) {
+                    onVertexDelete(e);
+                }
+                break;
+            case "edge":
+                if (e.evt.button === 0) {
+                    onEdgeCreate(e);
+                }
+                break;
+            case "color":
+                if (e.evt.button === 0) {
+                    onVertexRecolor(e, false);
+                }
+                else if (e.evt.button === 1) {
+                    onVertexRecolor(e, true);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    const onEdgeClick = (e) => {
+        switch (mode) {
+            case "edge":
+                if (e.evt.button === 1) {
+                    onEdgeDelete(e);
+                }
+                break;
+            case "color":
+                if (e.evt.button === 0) {
+                    onEdgeRecolor(e, false);
+                }
+                else if (e.evt.button === 1) {
+                    onVertexRecolor(e, true);
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     // Vertices
@@ -39,6 +102,50 @@ export default function Sketchpad({ mode, height, width }) {
         );
     };
 
+    const onVertexDragMove = (e) => {
+        const vertexMoving = vertices.filter((vertex) => {
+            return vertex.isDragging === true;
+        })[0];
+
+        const childVertex = e.target.getChildren((child) => {
+            return child.getClassName() === "Circle";
+        })[0];
+
+        if (vertexMoving !== undefined) {
+            setEdges(
+                edges.map((edge) => {
+                    if (vertexMoving.adjacencies.includes(edge.id)) {
+                        // if loop (edge starts ane ends at itself)
+                        if (edge.adjacencies[0] === edge.adjacencies[1]) {
+                            return {
+                                ...edge,
+                                points: [e.target.position().x + childVertex.position().x + edge.offset[0], e.target.position().y + childVertex.position().y + edge.offset[1],
+                                e.target.position().x + childVertex.position().x + edge.offset[4], e.target.position().y + childVertex.position().y + edge.offset[5],
+                                e.target.position().x + childVertex.position().x + edge.offset[2], e.target.position().y + childVertex.position().y + edge.offset[3]]
+                            };
+                        }
+                        // else if not a loop, which end are we moving?
+                        else if (vertexMoving.id === edge.adjacencies[0]) {
+                            return {
+                                ...edge,
+                                points: [e.target.position().x + childVertex.position().x + edge.offset[0], e.target.position().y + childVertex.position().y + edge.offset[1], edge.points[2], edge.points[3]]
+                            };
+                        }
+                        else {
+                            return {
+                                ...edge,
+                                points: [edge.points[0], edge.points[1], e.target.position().x + childVertex.position().x + edge.offset[2], e.target.position().y + childVertex.position().y + edge.offset[3]]
+                            };
+                        }
+                    }
+                    else {
+                        return { ...edge };
+                    }
+                })
+            );
+        }
+    }
+
     const onVertexDragEnd = (e) => {
         setVertices(
             vertices.map((vertex) => {
@@ -53,27 +160,176 @@ export default function Sketchpad({ mode, height, width }) {
     const onVertexCreate = (e) => {
         var pos = e.target.getStage().getPointerPosition();
         setVertices(
-            [...vertices, { id: idCounter.toString(), x: pos.x, y: pos.y, radius: 50, color: "red", isDragging: false }]
+            [...vertices, {
+                id: idCounter.toString(),
+                x: pos.x,
+                y: pos.y,
+                radius: 45,
+                color: color,
+                textColor: 'black',
+                isDragging: false,
+                adjacencies: [],
+            }]
         );
 
         setIDCounter(idCounter + 1);
     }
 
+    const onVertexDelete = (e) => {
+        // get the vertex you're wanting to delete
+        const vertexToDelete = vertices.filter((vertex) => {
+            return vertex.id === e.target.id();
+        })[0];
+
+        // remove edges connected to vertex we're about to delete
+        setEdges(
+            edges.filter((edge) => {
+                return !vertexToDelete.adjacencies.includes(edge.id);
+            })
+        );
+        // then delete vertex
+        setVertices(
+            vertices.filter((vertex) => {
+                return vertex.id !== e.target.id();
+            })
+        );
+    }
+
+    const onVertexRecolor = (e, reset) => {
+        setVertices(
+            vertices.map((vertex) => {
+                if (vertex.id === e.target.id()) {
+                    if (e.target.attrs.name === "text") {
+                        return { ...vertex, textColor: reset ? '#000000' : color };
+                    }
+                    else {
+                        return { ...vertex, color: reset ? '#FFFFFF' : color };
+                    }
+                }
+                else {
+                    return { ...vertex };
+                }
+            })
+        );
+    }
+
+    // update vertex count
+    React.useEffect(() => {
+        setVertexCount(vertices.length);
+    }, [setVertexCount, vertices.length]);
+
     // Edges
     const [edges, setEdges] = React.useState([]);
-    const [firstPos, setFirstPos] = React.useState({ x: -1, y: -1 });
+    const [firstPos, setFirstPos] = React.useState({ x: -1, y: -1, offsetx: -1, offsety: -1, id: -1 });
 
     const onEdgeCreate = (e) => {
-        // if negative one, then set this as the beginning of the line
-        if (firstPos.x == -1) {
-            var pos = e.target.getStage().getPointerPosition();
-            setFirstPos(pos);
-        }
-        else {
+        var currentPos = e.target.getStage().getPointerPosition();
 
-            setFirstPos({ x: -1, y: -1 });
+        // if negative one, then set this as the beginning of the line
+        if (firstPos.x === -1) {
+            setFirstPos({ x: currentPos.x, y: currentPos.y, offsetx: currentPos.x - e.target.position().x, offsety: currentPos.y - e.target.position().y, id: e.target.id() });
+            setVertices(
+                vertices.map((vertex) => {
+                    if (vertex.id === e.target.id()) {
+                        return {
+                            ...vertex,
+                            adjacencies: [...vertex.adjacencies, idCounter.toString()],
+                        }
+                    }
+                    else {
+                        return { ...vertex };
+                    }
+                })
+            );
+        }
+        // otherwise, create a new edge and add it
+        else {
+            var sameVertex = firstPos.id === e.target.id();
+            var sameVertexPoints = null;
+            if (sameVertex) {
+                sameVertexPoints = createEdgeLoop(currentPos, e.target.position());
+            }
+            setEdges(
+                [...edges, {
+                    id: idCounter.toString(),
+                    points: sameVertex ? sameVertexPoints : [firstPos.x, firstPos.y, currentPos.x, currentPos.y],
+                    tension: sameVertex ? 1 : 0,
+                    color: color,
+                    width: 10,
+                    adjacencies: [firstPos.id, e.target.id()],
+                    offset: sameVertex ?
+                        [firstPos.offsetx, firstPos.offsety, currentPos.x - e.target.position().x, currentPos.y - e.target.position().y,
+                        sameVertexPoints[2] - e.target.position().x, sameVertexPoints[3] - e.target.position().y] :
+                        [firstPos.offsetx, firstPos.offsety, currentPos.x - e.target.position().x, currentPos.y - e.target.position().y],
+                }]
+            );
+            setVertices(
+                vertices.map((vertex) => {
+                    if (vertex.id === e.target.id()) {
+                        return {
+                            ...vertex,
+                            adjacencies: [...vertex.adjacencies, idCounter.toString()],
+                        }
+                    }
+                    else {
+                        return { ...vertex };
+                    }
+                })
+            );
+
+            setIDCounter(idCounter + 1);
+            // reset pos so we can create a new edge in future
+            setFirstPos({ x: -1, y: -1, offsetx: -1, offsety: -1, id: -1 });
         }
     }
+
+    const onEdgeDelete = (e) => {
+        const edgeToDelete = edges.filter((edge) => {
+            return edge.id === e.target.id();
+        })[0];
+
+        setVertices(
+            vertices.map((vertex) => {
+                return {
+                    ...vertex,
+                    adjacencies: vertex.adjacencies.filter(edgeID => edgeID !== edgeToDelete.id)
+                };
+            })
+        );
+        setEdges(
+            edges.filter((edge) => {
+                return edgeToDelete.id !== edge.id;
+            })
+        );
+    }
+
+    const createEdgeLoop = (currentPos, centerPos) => {
+        var xDif = currentPos.x - centerPos.x;
+        var yDif = currentPos.y - centerPos.y;
+        var offsetX = xDif > 30 ? xDif : (xDif > 0 ? 5 : xDif - 10);
+        var offsetY = yDif > 30 ? yDif : (yDif > 0 ? 0 : yDif);
+
+        return [firstPos.x, firstPos.y, (firstPos.x + currentPos.x) / 2 + offsetX, (firstPos.y + currentPos.y) / 2 + offsetY, currentPos.x, currentPos.y];
+    }
+
+    const onEdgeRecolor = (e, reset) => {
+        setEdges(
+            edges.map((edge) => {
+                if (edge.id === e.target.id()) {
+                    return { ...edge, color: reset ? '#FFFFFF' : color };
+                }
+                else {
+                    return { ...edge };
+                }
+            })
+        );
+    }
+
+    // update edge count
+    React.useEffect(() => {
+        setEdgeCount(edges.length);
+    }, [setEdgeCount, edges.length]);
+
 
     return (
         <div className={theme + "-sketchpad"} style={{ "width": width, "height": height }}>
@@ -82,31 +338,54 @@ export default function Sketchpad({ mode, height, width }) {
                 width={width}
             >
                 <Layer>
-                    <Background color={theme === "dark" ? "rgb(100, 100, 100)" : "white"} />
+                    <Background color={bgColor} />
                 </Layer>
                 <Layer>
-                    {edges.length != 0 ? edges.map((edge) => (
+                    {edges.length !== 0 ? edges.map((edge) => (
                         <Line
+                            key={edge.id}
+                            id={edge.id}
                             points={edge.points}
                             stroke={edge.color}
                             strokeWidth={edge.width}
+                            tension={edge.tension}
+                            onMouseDown={onEdgeClick}
+                            perfectDrawEnabled={false}
                         />
                     )) : <Group />}
 
-                    {vertices.length != 0 ? vertices.map((vertex) => (
-                        <Circle
+                    {vertices.length !== 0 ? vertices.map((vertex) => (
+                        <Group
+                            draggable
                             key={vertex.id}
                             id={vertex.id}
-                            x={vertex.x}
-                            y={vertex.y}
-                            radius={vertex.radius}
-                            fill={vertex.color}
-                            draggable
                             onDragStart={onVertexDragStart}
                             onDragEnd={onVertexDragEnd}
-                            scaleX={vertex.isDragging ? 1.2 : 1}
-                            scaleY={vertex.isDragging ? 1.2 : 1}
-                        />
+                            onDragMove={onVertexDragMove}
+                            onMouseDown={onVertexClick}
+                        >
+                            <Circle
+                                id={vertex.id}
+                                name={"vertex"}
+                                x={vertex.x}
+                                y={vertex.y}
+                                radius={vertex.radius}
+                                fill={vertex.color}
+                                scaleX={vertex.isDragging ? 1.2 : 1}
+                                scaleY={vertex.isDragging ? 1.2 : 1}
+                                perfectDrawEnabled={false}
+                            />
+                            <Text
+                                id={vertex.id}
+                                name={"text"}
+                                x={vertex.x}
+                                y={vertex.y}
+                                text={vertex.id}
+                                fontSize={30}
+                                fill={vertex.textColor}
+                                perfectDrawEnabled={false}
+                            />
+                        </Group>
                     )) : <Group />}
                 </Layer>
             </Stage>
--- sketchpad/src/components/sketchpad/sketchpadLayout.js
+++ sketchpad/src/components/sketchpad/sketchpadLayout.js
@@ -10,6 +10,8 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Stack from 'react-bootstrap/Stack';
 
+import { GithubPicker } from 'react-color';
+
 /*
  * Organizes the various components of the sketchpad layout
  */
@@ -17,25 +19,91 @@ export default function SketchpadLayout() {
     const theme = React.useContext(ThemeContext).theme;
 
     const [mode, setMode] = React.useState("vertex");
+    const [color, setColor] = React.useState("#FFF");
+
+    const [vertexCount, setVertexCount] = React.useState(0);
+    const [edgeCount, setEdgeCount] = React.useState(0);
+
+    const userSelectColor = (selectedColor) => {
+        setColor(selectedColor.hex);
+    }
+
+    React.useEffect(() => {
+        console.log(mode);
+    }, [mode]);
+
+    const onKeyDown = (e) => {
+        switch (e.key) {
+            case 'v':
+                setMode("vertex");
+                break;
+            case 'e':
+                setMode("edge");
+                break;
+            case 'c':
+                setMode("color");
+                break;
+            case 'w':
+                setColor('#FFFFFF');
+                break;
+            case 'b':
+                setColor('#000000');
+                break;
+            case 'r':
+                setColor('#B80000');
+                break;
+            case 'o':
+                setColor('#DB3E00');
+                break;
+            case 'y':
+                setColor('#FCCB00');
+                break;
+            case 'g':
+                setColor('#008B02');
+                break;
+            case 'l':
+                setColor('#1273DE');
+                break;
+            case 'p':
+                setColor('#5300EB');
+                break;
+            default:
+                break;
+        }
+    };
 
     return (
-        <Container className={theme}>
-            <Row>
-                <Col><div style={{ "fontSize": "35px", "padding": "1%" }}>Graph Theorist's Sketchpad</div></Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Stack gap={3} style={{ "padding": "10%" }} vertical="true">
-                        <Button id="vertex" onChange={(e) => setMode(e.target.id)} variant={theme === "dark" ? "light" : "dark"}>Vertex Mode (v)</Button>
-                        <Button id="edge" onChange={(e) => setMode(e.target.id)} variant={theme === "dark" ? "light" : "dark"}>Edge Mode (e)</Button>
-                    </Stack>
-                </Col>
-                <Col><Sketchpad mode={mode} height={window.innerHeight / 1.5} width={window.innerWidth / 2} /></Col>
-                <Col></Col>
-            </Row>
-            <Row>
-                <Col></Col>
-            </Row>
-        </Container>
+        <div tabIndex={0} onKeyDown={onKeyDown}>
+            <Container className={theme}>
+                <Row>
+                    <Col><div style={{ "fontSize": "35px", "padding": "1%" }}>Graph Theorist's Sketchpad</div></Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Stack gap={3} style={{ "padding": "10%" }} vertical="true">
+                            <Button id="vertex" onClick={(e) => setMode(e.target.id)} variant={theme === "dark" ? "light" : "dark"} active={mode === "vertex"}>Vertex Mode (v)</Button>
+                            <Button id="edge" onClick={(e) => setMode(e.target.id)} variant={theme === "dark" ? "light" : "dark"} active={mode === "edge"}>Edge Mode (e)</Button>
+                            <Button id="color" onClick={(e) => setMode(e.target.id)} variant={theme === "dark" ? "light" : "dark"} active={mode === "color"}>Recolor Mode (c)</Button>
+                            <div style={{ "margin": "auto" }}>
+                                <GithubPicker onChangeComplete={userSelectColor} width={'240px'} triangle={'hide'}
+                                    color={color}
+                                    colors={['#FFFFFF', '#B80000', '#DB3E00', '#FCCB00', '#008B02', '#006B76', '#1273DE', '#004DCF', '#5300EB', '#000000', '#EB9694', '#FAD0C3', '#FEF3BD', '#C1E1C5', '#BEDADC', '#C4DEF6', '#BED3F3', '#D4C4FB']} />
+                            </div>
+                        </Stack>
+                    </Col>
+                    <Col><Sketchpad mode={mode} color={color} setVertexCount={setVertexCount} setEdgeCount={setEdgeCount} height={window.innerHeight / 1.5} width={window.innerWidth / 2} /></Col>
+                    <Col>
+                        <Stack vertical="true">
+                            <h3>Stats</h3>
+                            <p style={{ "marginBottom": "1%" }}>Vertex Count: {vertexCount}</p>
+                            <p>Edge Count: {edgeCount}</p>
+                        </Stack>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col></Col>
+                </Row>
+            </Container>
+        </div>
     )
 }
